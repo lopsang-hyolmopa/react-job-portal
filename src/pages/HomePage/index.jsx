a5642d457be3508ddb52189085ef45899289b540
lopsang-hyolmopa <lopsang.lama.comparewheels@gmail.com>
2024-04-04 22:24:24 +0545

    CTA section

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,12 @@
 import Hero from "./Hero";
 import RecentJobs from "./RecentJobs";
+import CTASection from "./CTASection";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <CTASection />
       <RecentJobs />
     </>
   );
